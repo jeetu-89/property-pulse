1bc41421f76791a7196bc85fd04dca3602901bbc
jeetu <jitendradew2003@gmail.com>
2025-09-18 11:57:00 +0530

    Add AddPropertyForm skeleton

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,5 +1,15 @@
+import PropertyAddForm from "@/components/PropertyADDForm";
+
 const AddPropertyPage = () => {
-  return <div>Add </div>;
+  return (
+    <section className="bg-blue-50">
+      <div className=" py-24 container max-w-2xl mx-auto">
+        <div className="bg-white m-4 border shadow-md px-6 py-8 md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AddPropertyPage;
