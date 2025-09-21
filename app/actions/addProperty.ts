2f64c382b4c373190d23a71c67e92b2d3c7db6cd
jeetu <jitendradew2003@gmail.com>
2025-09-21 17:39:11 +0530

    Add session as owner at Property database

--- app/actions/addProperty.ts
+++ app/actions/addProperty.ts
@@ -1,12 +1,19 @@
 "use server";
 
 import connectDB from "@/config/db";
+import Property from "@/models/Property";
 import getSessionUser from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 async function addProperty(formData: FormData) {
-    await connectDB();
+  await connectDB();
 
-    const sessionUser = await getSessionUser(); 
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required.");
+  }
+  const { userId } = sessionUser;
   const amenities = formData.getAll("amenities");
   const images = formData
     .getAll("images")
@@ -14,6 +21,7 @@ async function addProperty(formData: FormData) {
     .map((image) => image instanceof File && image.name);
 
   const propertyData = {
+    owner: userId,
     type: formData.get("type"),
     name: formData.get("name"),
     description: formData.get("description"),
@@ -39,7 +47,11 @@ async function addProperty(formData: FormData) {
     },
     images,
   };
-  console.log(propertyData);
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath("/", "layout");
+  redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
