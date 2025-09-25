9b484f4fc1f87a8747dc75f8818e8d8276a98701
jeetu <jitendradew2003@gmail.com>
2025-09-26 01:28:51 +0530

    Make Bookmark Property fully functional

---
+++ app/actions/checkBookmarkStatus.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import User, { UserTypeHydrated } from "@/models/User";
+import getSessionUser from "@/utils/getSessionUser";
+import { Types } from "mongoose";
+
+const checkBookmarkStatus = async (propertyId: string) => {
+  const propertyObjectId = new Types.ObjectId(propertyId);
+
+  const userSession = await getSessionUser();
+  if (!userSession || !userSession.userId) {
+    throw new Error("User Id is not present.");
+  }
+  const { userId } = userSession;
+
+  const user = await User.findById<UserTypeHydrated>(userId);
+  if (!user) {
+    throw new Error("Property not found.");
+  }
+  const isBookmarked = user.booksmarks.some((bookmark) =>
+    bookmark.equals(propertyObjectId)
+  );
+  return { isBookmarked };
+};
+
+export default checkBookmarkStatus;
--- components/BookmarkButton.tsx
+++ components/BookmarkButton.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 import type { PropertyType } from "@/models/Property";
 import type { SerializableObjectType } from "@/utils/convertToObjext";
-import { error } from "console";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { FaBookmark } from "react-icons/fa";
 import { toast } from "react-toastify";
 
@@ -15,13 +16,19 @@ const BookmarkButton = ({
 }) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
   const handleClick = async () => {
     if (!userId) {
       toast.error("You need to be signed in to bookmark a property.");
       return;
     }
     bookmarkProperty(property._id)
-      .then((res) => toast.success(res.message))
+      .then((res) => {
+        toast.success(res.message);
+        setIsBookmarked(res.isBookmarked);
+      })
       .catch((err) => {
         const mssg =
           err instanceof Error
@@ -30,7 +37,37 @@ const BookmarkButton = ({
         toast.error(mssg);
       });
   };
-  return (
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+    checkBookmarkStatus(property._id.toString())
+      .then((res) => {
+        setIsBookmarked(res.isBookmarked);
+        setLoading(false);
+      })
+      .catch((err) => {
+        const message =
+          err instanceof Error
+            ? err.message
+            : "Unable to check bookmark status";
+        toast.error(message);
+        setLoading(false);
+      });
+  }, [userId, property._id, checkBookmarkStatus]);
+  if (loading) {
+    return <p className="text-center">Loading...</p>;
+  }
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2"></FaBookmark> Remove Bookmark
+    </button>
+  ) : (
     <button
       onClick={handleClick}
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
